80cc50e3179094f13c62007e01de508f446fa2bc
vrajmandaliya396-spec <vrajmandaliya396@gmail.com>
2026-03-18 15:18:52 +0530

    initial commit

---
+++ test2.py
@@ -0,0 +1,25 @@
+import cv2, mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+lmList = []
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cv2.flip(img, 1, img)
+    cv2.imshow('frame', img)
+
+    print(results)
+    if cv2.waitKey(1) == ord('q'):
+        break
